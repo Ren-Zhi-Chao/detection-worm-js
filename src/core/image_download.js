9a7cf257930aaf504b1b94f9497e1a219e8c3b32
zhichao.ren <973063956@qq.com>
2020-03-20 15:17:50 +0800

    添加图片下载功能

---
+++ src/core/image_download.js
@@ -0,0 +1,58 @@
+import request from 'request';
+import fs from 'fs';
+import _path from 'path';
+import $ from './object_type';
+
+class ImageDownload {
+
+    constructor(url, filename, path = process.cwd()) {
+        this.url = url;
+        this.file = _path.join(path, filename);
+        this.writeStream = fs.createWriteStream(this.file);
+    }
+
+    setOptions(options = { method: 'get', headers: { referer: this.url } }) {
+        this.options = options;
+        return this;
+    }
+
+    init() {
+        if ($.isMap(this.options)) {
+            this.options.url = this.url;
+        } else {
+            this.options = { url: this.url };
+        }
+        return this;
+    }
+
+    download(callback = { end: null, error: null, success: null }) {
+        const http = request(this.options);
+        http.pipe(this.writeStream);
+        http.on('end', function() {
+            this.writeStream.end();
+            if ($.isFunction(callback.success)) {
+                callback.success();
+            } else {
+                console.log('文件下载成功~')
+            }
+        });
+        http.on('error', function(err) {
+            this.writeStream.end();
+            if ($.isFunction(callback.error)) {
+                callback.error();
+            } else {
+                console.log("错误信息:" + err)
+            }
+        })
+        http.on("finish", function() {
+            this.writeStream.end();
+            if ($.isFunction(callback.end)) {
+                callback.end();
+            } else {
+                console.log("文件写入成功");
+            }
+        });
+    }
+}
+
+export default ImageDownload;
